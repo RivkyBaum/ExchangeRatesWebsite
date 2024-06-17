352163e4156ede69034900400c040d7b7518a684
RivkyBaum <rivky3254@gmail.com>
2024-06-17 21:18:06 +0300

    env

--- ExchangeRatesClient/src/app/service/ExchangeRates.service.ts
+++ ExchangeRatesClient/src/app/service/ExchangeRates.service.ts
@@ -2,20 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Currency } from '../Classes/Currency';
+import { environment } from "src/environment";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExchangeRatesService {
 
-  private apiUrl = 'https://localhost:7127/api/ExchangeRates';
+  // private apiUrl = 'https://localhost:7127/api/ExchangeRates';
 
   constructor(private http: HttpClient) { }
 
   getCurrencyExchangeRates(currencyName: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/exchangeRates/${currencyName}`); 
+    return this.http.get<any>(`${environment.apiUrl}/exchangeRates/${currencyName}`); 
   }
   getAllCurrencies(): Observable<Currency[]> {
-    return this.http.get<any>(`${this.apiUrl}/AllCurrencies`);
+    return this.http.get<any>(`${environment.apiUrl}/AllCurrencies`);
   }
 }
